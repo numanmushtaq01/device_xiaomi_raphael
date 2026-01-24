4204cf32dbc6a663d80f9bcb474dfe733afa93bb
numanmushtaq01 <naveenmushtaq3@gmail.com>
2026-01-25 03:08:10 +0530

    raphael: raphael: Remove vendor/lineage device framework matrix inclusion

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,11 +56,10 @@ TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 338
 TARGET_USES_FOD_ZPOS := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 	$(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml 
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_SKUS += nfc
